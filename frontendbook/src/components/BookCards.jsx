bc3837d50b9d0320bdc74db184385e785604825c
dengluxurye <dluxurymou@gmail.comgit config --global user.email dluxurymou@gmail.com>
2025-03-25 09:05:02 +0200

    changes

--- frontendbook/src/components/BookCards.jsx
+++ frontendbook/src/components/BookCards.jsx
@@ -47,7 +47,7 @@ const BookCards = ({headline, books}) => {
         books.map(book => <SwiperSlide key={book._id}>
         <Link to={`/book/${book._id}`}>
             <div className='relative'>
-                <img src={book.URL} alt="" />
+                <img src={book.imageURL} alt="" />
                 <div className='absolute top-3 right-3 bg-blue-600 hover:bg-black p-2 rounded'>
                     <FaCartShopping className='w-4 h-4 text-white'/>
                 </div>
